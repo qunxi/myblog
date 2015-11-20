fcdc4cfa6022c429cb6971b9ae259a6fb3ba0c9a
qunxi <wangqunxi@gmail.com>
2015-11-20 18:11:52 +0800

    fixed layout mass

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -3,6 +3,22 @@ angular.module('app', []);
 angular.module('app').constant('API_URL', 'http://www.wangqunxi.com/api/');
 //angular.module('app').constant('API_URL', 'http://localhost:3000/api/');
 
+angular.module('app').factory('UtilsService', UtilsService);
+function UtilsService(){
+	var service = {
+		cutString: cutString
+	};
+
+	function cutString(data, maxLength){
+		if(data.length > maxLength){
+			return data.substr(0, maxLength) + '...';
+		}
+		return data;
+	}
+
+	return service;
+}
+
 angular.module('app').factory('PostService', PostService);
 PostService.$inject = ['$http', 'API_URL'];
 function PostService($http, API_URL){
@@ -28,8 +44,8 @@ function PostService($http, API_URL){
 
 
 angular.module('app').controller('PostCtrl', PostCtrl);
-PostCtrl.$inject = ['PostService'];
-function PostCtrl(PostService){
+PostCtrl.$inject = ['$sce', 'PostService', 'UtilsService'];
+function PostCtrl($sce, PostService, UtilsService){
 	var vm = this;
 	vm.posts = [];
 	vm.showMorePosts = showMorePosts;
@@ -39,8 +55,14 @@ function PostCtrl(PostService){
 	function initPosts(){
 		PostService.getPostsByDate(0, 6)
 				   .then(function(data){
-				   		
-				   	 	vm.posts = data;
+				   	 	vm.posts = _.map(data, function(n){
+				   	 		if(n.description){
+				   	 			console.log(UtilsService.cutString(n.description, 45));
+				   	 			n.description = $sce.trustAsHtml(UtilsService.cutString(n.description, 45));
+				   	 		}
+				   	 		return n;
+				   	 	});
+				   	 	console.log(vm.posts);
 				   });
 	}
 
